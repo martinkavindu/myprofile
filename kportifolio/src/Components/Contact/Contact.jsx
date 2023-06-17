6ccc80880574526f5f76bbd1fa806ff7fdcb1abf
mutuku1998 <martinmutuku1998@gmail.com>
2023-06-17 20:59:41 +0300

    emailjsworking

--- kportifolio/src/Components/Contact/Contact.jsx
+++ kportifolio/src/Components/Contact/Contact.jsx
@@ -1,54 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Contact.css';
-import  { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import emailjs from 'emailjs-com';
 import { themeContext } from '../../Context';
 import { useContext } from 'react';
 
+
 const Contact = () => {
     const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
-    const form = useRef();
-    const [done,setDone] = useState(false)
+    const darkMode = theme.state.darkMode;
+  const form = useRef();
+  const [done, setDone] = useState(false);
 
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_sx6a06f', 'template_98m7ktd', form.current, '08mPFcjipVydQw64F')
-        .then((result) => {
-            console.log(result.text);
-            setDone(true);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
+  useEffect(() => {
+    emailjs.init('YOUR_USER_ID');
+  }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_pj9cbxo', 'template_98m7ktd', form.current, '08mPFcjipVydQw64F')
+      .then((result) => {
+        console.log(result.text);
+        setDone(true);
+        form.current.reset(); // Reset the form fields
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <div className='contact-form'>
-    <div className='w-left'>
+      <div className='w-left'>
         <div className='awesome'>
-            <span style={{color:darkMode? 'white':''}}>Get in touch</span>
-            <span>Contact Me</span>
-            <div className='blur s-blur1'
-            style={{
-                background: "#ABF1FF94"
-            }}></div>
+          <span style={{color:darkMode? 'white':''}}>Get in touch</span>
+          <span>Contact Me</span>
+          <div className='blur s-blur1' style={{ background: '#ABF1FF94' }}></div>
         </div>
-    </div>
-    <div className='c-right'>
-        <form  ref={form} onSubmit={sendEmail} >
-            <input type='text'name ="user_name" className='user' placeholder='Name'/>
-            <input type='email' name='user_email'className='user' placeholder='Email'/>
-            <textarea name="message" className='user' placeholder='Message'/>
-            <input type='submit'value="send" className='button'/>
-            <span>{done && "Thanks for your email"}</span>
-<div className='blur c-blur1'
-style={{background:"var(--purple)"}}>
-</div>
+      </div>
+      <div className='c-right'>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='user_name' className='user' placeholder='Name' required />
+          <input type='email' name='user_email' className='user' placeholder='Email' required />
+          <textarea name='message' className='user' placeholder='Message' required />
+          <input type='submit' value='Send' className='button' />
+          <span>{done && 'Thanks for your email'}</span>
+          <div className='blur c-blur1' style={{ background: 'var(--purple)' }}></div>
         </form>
+      </div>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Contact
+export default Contact;
